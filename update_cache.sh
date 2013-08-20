ead63866b1ca4a202fae0a5f3f0b05dadbc7b438
Chetan Sarva <chetan@pixelcop.net>
2013-08-19 23:21:56 -0400

    make sure libv8 is avail for linux

--- update_cache.sh
+++ update_cache.sh
@@ -28,6 +28,16 @@ cd $ROOT/src/manager
 echo "* updating manager gem cache"
 bundle package --all >/dev/null
 
+# make sure we have libv8 avail for linux
+# https://rubygems.org/downloads/libv8-3.11.8.17-x86_64-linux.gem
+cd $ROOT/src/manager/vendor/cache
+v8=`ls libv8* | grep darwin | xargs basename | perl -ne 's/-x86.*$//g; print $_'`
+ver="$v8-x86_64-linux"
+if [ ! -f $ver.gem ]; then
+  url="https://rubygems.org/downloads/$ver.gem"
+  wget -q $url
+fi
+
 # update test gem cache
 cd $ROOT/tests
 echo "* updating test gem cache"
