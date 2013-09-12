0bb409289dc34656f906a04bb14e0f48e3f46799
Chetan Sarva <chetan@pixelcop.net>
2013-09-12 15:22:04 -0400

    fix libv8 when running on linux

--- scripts/update_cache.sh
+++ scripts/update_cache.sh
@@ -31,11 +31,14 @@ bundle package --all >/dev/null
 # make sure we have libv8 avail for linux
 # https://rubygems.org/downloads/libv8-3.11.8.17-x86_64-linux.gem
 cd $ROOT/src/manager/vendor/cache
-v8=`ls libv8* | grep darwin | xargs basename | perl -ne 's/-x86.*$//g; print $_'`
-ver="$v8-x86_64-linux"
-if [ ! -f $ver.gem ]; then
-  url="https://rubygems.org/downloads/$ver.gem"
-  wget -q $url
+if [[ ! `ls libv8* 2> /dev/null | grep linux` ]]; then
+  # running on darwin, wget the gem
+  v8=`ls libv8* | grep darwin | xargs basename | perl -ne 's/-x86.*$//g; print $_'`
+  ver="$v8-x86_64-linux"
+  if [ ! -f $ver.gem ]; then
+    url="https://rubygems.org/downloads/$ver.gem"
+    wget -q $url
+  fi
 fi
 
 # update test gem cache
