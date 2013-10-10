a628bb68fa702fe5c74df29d8a7e2b3b3de71406
Chetan Sarva <chetan@pixelcop.net>
2013-10-10 16:03:30 -0400

    fixed reset

--- scripts/reset_manager.sh
+++ scripts/reset_manager.sh
@@ -3,10 +3,10 @@
 # stop manager and delete all data
 
 sudo RAILS_ENV=staging god terminate
-cd /var/www/bixby/current
-rake db:drop
+[ -f /var/www/bixby/current/Rakefile ] && cd /var/www/bixby/current && rake db:drop
+cd
 redis-cli flushall
-rm -rf /var/www/bixby/
+sudo rm -rf /var/www/bixby/
 
 # reinstall
 /opt/bixby-integration/scripts/install_manager.sh
