9f90c71c394c362ba37840bac72f19bf7cd677b8
Chetan Sarva <chetan@pixelcop.net>
2013-10-11 10:42:00 -0400

    force kill manager procs

--- scripts/reset_manager.sh
+++ scripts/reset_manager.sh
@@ -3,6 +3,8 @@
 # stop manager and delete all data
 
 sudo RAILS_ENV=staging god terminate
+sudo pkill -9 -u 0 -U 0 -f sidekiq
+sudo pkill -9 -u 0 -U 0 -f puma
 [ -f /var/www/bixby/current/Rakefile ] && cd /var/www/bixby/current && rake db:drop
 cd
 redis-cli flushall
