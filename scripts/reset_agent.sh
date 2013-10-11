28d08a54a99275daa3f7373a1c82b63836b29840
Chetan Sarva <chetan@pixelcop.net>
2013-10-11 10:46:39 -0400

    force kill agent procs

--- scripts/reset_agent.sh
+++ scripts/reset_agent.sh
@@ -2,6 +2,9 @@
 
 # stop current agent & cleanup
 sudo /etc/init.d/bixby stop
+sudo pkill -9 -u 0 -U 0 -f god
+sudo pkill -9 -u 0 -U 0 -f bixby-agent
+sudo pkill -9 -u 0 -U 0 -f mon_daemon
 cd /opt/bixby/etc
 sudo rm -f bixby.yml id_rsa server.pub
 cd ../var/
