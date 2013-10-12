01aaff2c956efb9b630a558de89f8175e1233e41
Chetan Sarva <chetan@pixelcop.net>
2013-10-12 00:25:00 -0400

    fix manager stop

--- scripts/manager/stop.sh
+++ scripts/manager/stop.sh
@@ -2,8 +2,9 @@
 
 # stop manager, cleanup databases
 sudo RAILS_ENV=staging god terminate
-sudo pkill -9 -u 0 -U 0 -f sidekiq
-sudo pkill -9 -u 0 -U 0 -f puma
+sudo pkill -9 -u 0 -U 0 -f god
+pkill -9 -f sidekiq
+pkill -9 -f puma
 [ -f /var/www/bixby/current/Rakefile ] && cd /var/www/bixby/current && rake db:drop
 redis-cli flushall > /dev/null
 rm -rf /var/www/bixby/shared/bixby/*
