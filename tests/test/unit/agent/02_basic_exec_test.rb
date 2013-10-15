62b821c0f266381dbebff40502f5ef60e023fb4f
Chetan Sarva <chetan@pixelcop.net>
2013-10-15 14:26:36 -0400

    tests for update version and list facts

--- tests/test/unit/agent/02_basic_exec_test.rb
+++ tests/test/unit/agent/02_basic_exec_test.rb
@@ -28,8 +28,6 @@ class Integration::Agent::BasicExec < Bixby::Test::TestCase
   end
 
   def test_hello_world
-
-    # tell the manager to execute hello world
     cmd = CommandSpec.new({
       :repo    => "vendor",
       :bundle  => "system/general",
@@ -52,6 +50,27 @@ class Integration::Agent::BasicExec < Bixby::Test::TestCase
     ver = File.read(Dir.glob("/opt/bixby/embedded/lib/ruby/gems/*/gems/bixby-agent-*/VERSION").first).strip
     refute_empty ver
     assert_equal ver, cr.stdout.strip
+  end
+
+  def test_update_agent_version
+    req = JsonRequest.new("inventory:update_version", [@agent_id])
+    res = Bixby.client.exec_api(req)
+    assert res
+    assert res.success?
+    assert_equal true, res.data
+  end
+
+  def test_list_facts
+    cmd = CommandSpec.new({
+      :repo    => "vendor",
+      :bundle  => "system/inventory",
+      :command => "list_facts.rb",
+    })
+    cr = exec(cmd)
+    facts = MultiJson.load(cr.stdout)
+    assert_kind_of Hash, facts
+    assert_equal "Linux", facts["kernel"]
+    assert_equal "bixbytest", facts["hostname"]
   end
 
 end
