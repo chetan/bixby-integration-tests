fd7ec8c6cd392f186ada2df34825ec2b5e102642
Chetan Sarva <chetan@pixelcop.net>
2013-10-15 16:43:17 -0400

    more monitoring tests; always wait for daemon to start

--- tests/test/unit/agent/03_monitoring_test.rb
+++ tests/test/unit/agent/03_monitoring_test.rb
@@ -41,7 +41,7 @@ class Integration::Agent::Monitoring < Bixby::Test::AgentTestCase
 
     # check should have been written to config.json as well, verify it
     assert wait_for_file_change("/opt/bixby/etc/monitoring/config.json", @start_time, 10)
-    sleep 0.2 # delay while the file is getting written?
+    sleep 0.2 # small delay to avoid race while file being written
     mon_config = MultiJson.load(File.read("/opt/bixby/etc/monitoring/config.json"))
     assert_kind_of Array, mon_config
     mon_config = mon_config.last
@@ -52,6 +52,19 @@ class Integration::Agent::Monitoring < Bixby::Test::AgentTestCase
     assert_equal cmd[:command], mon_config["command"]["command"]
 
     data
+  end
+
+  # Wait for the monitoring daemon to come up
+  def wait_for_mon_daemon
+    timeout(10) {
+      while true do
+        shell = systemu("ps auxw | grep -v grep | grep mon_daemon.rb")
+        next if shell.stdout.empty?
+        ps = shell.stdout.split(/\s+/)
+
+        break if ps.last == "mon_daemon.rb" && ps.first == "bixby"
+      end
+    }
   end
 
   def test_add_first_check_cpu_load
@@ -65,17 +78,7 @@ class Integration::Agent::Monitoring < Bixby::Test::AgentTestCase
     assert check
     assert check["name"] =~ /load average/i
 
-    # mon_daemon should be running now-ish..
-    timeout(10) {
-      while true do
-        shell = systemu("ps auxw | grep -v grep | grep mon_daemon.rb")
-        next if shell.stdout.empty?
-        ps = shell.stdout.split(/\s+/)
-
-        break if ps.last == "mon_daemon.rb" && ps.first == "bixby"
-      end
-    }
-
+    wait_for_mon_daemon()
   end
 
   def test_add_cpu_usage
@@ -88,6 +91,64 @@ class Integration::Agent::Monitoring < Bixby::Test::AgentTestCase
 
     assert check
     assert check["name"] =~ /cpu usage/i
+
+    wait_for_mon_daemon()
+  end
+
+  def test_add_disk_usage
+
+    check = add_check({
+      :repo    => "vendor",
+      :bundle  => "hardware/storage",
+      :command => "monitoring/disk_usage.rb",
+    }, nil)
+
+    assert check
+    assert check["name"] =~ /disk usage/i
+
+    wait_for_mon_daemon()
+  end
+
+  def test_add_inode_usage
+
+    check = add_check({
+      :repo    => "vendor",
+      :bundle  => "hardware/storage",
+      :command => "monitoring/inode_usage.rb",
+    }, nil)
+
+    assert check
+    assert check["name"] =~ /inode usage/i
+
+    wait_for_mon_daemon()
+  end
+
+  # Test a direct call to update check API
+  def test_update_check_config
+
+    shell = systemu("ps auxw | grep -v grep | grep mon_daemon.rb")
+    ps = shell.stdout.split(/\s+/)
+    pid = ps[1]
+
+    req = JsonRequest.new("monitoring:update_check_config", [@agent_id])
+    res = Bixby.client.exec_api(req)
+    p res
+    assert res
+    assert res.success?
+
+    assert wait_for_file_change("/opt/bixby/etc/monitoring/config.json", @start_time, 10)
+
+    # mon_daemon should restart
+    timeout(10) {
+      while true do
+        shell = systemu("ps auxw | grep -v grep | grep mon_daemon.rb")
+        next if shell.stdout.empty?
+        ps = shell.stdout.split(/\s+/)
+
+        # check for pid change
+        break if ps.last == "mon_daemon.rb" && ps.first == "bixby" && ps[1] != pid
+      end
+    }
   end
 
 end
