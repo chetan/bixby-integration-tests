89dbb398762139ebf70758abbc87318b326a212e
Chetan Sarva <chetan@pixelcop.net>
2013-10-15 16:47:41 -0400

    fix mon_daemon reset

--- scripts/agent/stop.sh
+++ scripts/agent/stop.sh
@@ -4,8 +4,8 @@
 sudo /etc/init.d/bixby stop
 sudo pkill -9 -u 0 -U 0 -f god
 sudo pkill -9 -u 0 -U 0 -f bixby-agent
-sudo pkill -9 -u 0 -U 0 -f mon_daemon
+sudo pkill -9 -f mon_daemon
 cd /opt/bixby/etc
-sudo rm -f bixby.yml id_rsa server.pub
+sudo rm -f bixby.yml id_rsa server.pub god.d/m*.god
 cd ../var/
 sudo rm -rf *
