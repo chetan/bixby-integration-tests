2b25bb68908dd6dd5cb7d4f4a1528800ebc7eb83
Chetan Sarva <chetan@pixelcop.net>
2013-10-30 22:10:36 -0400

    fixed agent stop

--- scripts/agent/stop.sh
+++ scripts/agent/stop.sh
@@ -6,11 +6,11 @@ sudo pkill -9 -u 0 -U 0 -f god
 sudo pkill -9 -u 0 -U 0 -f bixby-agent
 sudo pkill -9 -f mon_daemon
 
-if [[ -f /opt/bixby/etc ]]; then
+if [[ -d /opt/bixby/etc ]]; then
   cd /opt/bixby/etc
   sudo rm -f bixby.yml id_rsa server.pub god.d/m*.god
 fi
 
-if [[ -f /opt/bixby/var ]]; then
+if [[ -d /opt/bixby/var ]]; then
   sudo rm -rf /opt/bixby/var/*
 fi
