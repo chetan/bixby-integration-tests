3d17297b6a9f9f1a3d9531beb8566b9f827dcb03
Chetan Sarva <chetan@pixelcop.net>
2013-11-07 18:21:16 -0500

    get rid of all the unused spork/zeus crap; make sure oj is used

--- tests/test/helper.rb
+++ tests/test/helper.rb
@@ -1,78 +1,28 @@
 
-def spork_running?
-  ENV.include? "DRB"
+root = File.expand_path(File.dirname(__FILE__))
+if not $:.include? root then
+  # add to library load path
+  $: << root
+end
+$: << File.join(File.dirname(root), "lib")
+
+require "rubygems"
+if not defined? ::Bundler then
+  require "bundler/setup"
 end
 
-def zeus_running?
-  File.exists? '.zeus.sock' and Module.const_defined?(:Zeus)
-end
+EasyCov.path = "coverage"
+EasyCov.filters << EasyCov::IGNORE_GEMS << EasyCov::IGNORE_STDLIB
+EasyCov.filters << lambda { |filename|
+  # ignore vendored files
+  filename !~ %r(#{EasyCov.root}/vendor/)
+}
+EasyCov.start
 
-def prefork
-  root = File.expand_path(File.dirname(__FILE__))
-  if not $:.include? root then
-    # add to library load path
-    $: << root
-  end
-  $: << File.join(File.dirname(root), "lib")
+require "micron/minitest"
 
-  require "rubygems"
-  if not defined? ::Bundler then
-    require "bundler/setup"
-  end
-
-  if not(spork_running? or zeus_running?) then
-    # load now if neither spork (DRB) or zeus are running
-    # (usually during manual rake test run)
-    load_simplecov()
-  end
-
-  require "test_guard"
-  require "micron/minitest"
-end
-
-def load_simplecov
-  EasyCov.path = "coverage"
-  EasyCov.filters << EasyCov::IGNORE_GEMS << EasyCov::IGNORE_STDLIB
-  EasyCov.filters << lambda { |filename|
-    # ignore vendored files
-    filename !~ %r(#{EasyCov.root}/vendor/)
-  }
-  EasyCov.start
-end
-
-def bootstrap_tests
-  if spork_running? or zeus_running? then
-    load_simplecov()
-  end
-
-  require "setup/base"
-  require "setup/agent_test_case"
-  require "setup/ui_test_case"
-  require "setup/logged_in_ui_test_case"
-end
-
-if Object.const_defined? :Spork then
-
-  #uncomment the following line to use spork with the debugger
-  #require 'spork/ext/ruby-debug'
-
-  Spork.prefork do
-    prefork()
-  end
-
-  Spork.each_run do
-    bootstrap_tests()
-  end
-
-  # Spork.after_each_run do
-  # end
-
-elsif zeus_running? then
-  prefork()
-
-else
-  # normal 'rake test'
-  prefork()
-  bootstrap_tests()
-
-end
+require "oj" # make sure we are using oj for multi_json
+require "setup/base"
+require "setup/agent_test_case"
+require "setup/ui_test_case"
+require "setup/logged_in_ui_test_case"
