b5dbf7d93807d2aab7dfa40c22061963a20501ff
Chetan Sarva <chetan@pixelcop.net>
2013-11-07 15:05:57 -0500

    log all requests made by phantomjs/poltergeist

--- tests/test/setup/ui_test_case.rb
+++ tests/test/setup/ui_test_case.rb
@@ -5,6 +5,13 @@ Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 5
 
+Capybara.register_driver :poltergeist do |app|
+  opts = {
+    # :logger           => $stdout,
+    :phantomjs_logger => $stdout,
+  }
+  Capybara::Poltergeist::Driver.new(app, opts)
+end
 
 
 class Capybara::Poltergeist::NetworkTraffic::Request
@@ -18,6 +25,46 @@ class Capybara::Poltergeist::NetworkTraffic::Response
   attr_reader :data
 end
 
+class ConsoleFilter
+
+  def initialize(real_out, filter=true)
+    @real_out = real_out
+    @filter = filter
+    @buffer = StringIO.new
+  end
+
+  def write(data)
+
+    if !@filter then
+      @real_out.write(data)
+      return
+    end
+
+    # get previous buffer (partial line)
+    b = @buffer.read
+    @buffer.rewind
+    if not b.empty? then
+      data = b + data
+    end
+
+    # read lines from data
+    buff = StringIO.new(data)
+    lines = buff.readlines
+    if lines then
+      if lines.last !~ /\n$/ then
+        # store partial lines for next write
+        @buffer.write(lines.pop)
+      end
+      lines.each{ |line|
+        if line =~ /^\[netlog\]/ then
+          @real_out.puts(line)
+        end
+      }
+    end
+
+  end
+end
+
 module Bixby
   module Test
     class UITestCase < TestCase
@@ -26,6 +73,11 @@ module Bixby
 
       def setup
         super
+        # make sure the logger gets updated to the correct object on each test run
+        # since Micron will replace $stdout/$stderr before each method is run
+        console_filter = ConsoleFilter.new($stdout, true)
+        page.driver.client.phantomjs_logger = console_filter if page.driver.client.phantomjs_logger
+        page.driver.browser.logger = out if page.driver.browser.logger
       end
 
       def teardown
