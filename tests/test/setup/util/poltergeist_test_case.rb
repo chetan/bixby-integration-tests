9668ae35520270f41365347d2e4468666fc7e1dd
Chetan Sarva <chetan@pixelcop.net>
2013-11-09 10:17:27 -0500

    after_class needs to be extended into the base

--- tests/test/setup/util/poltergeist_test_case.rb
+++ tests/test/setup/util/poltergeist_test_case.rb
@@ -3,14 +3,23 @@ module Micron
   class TestCase
     module Poltergeist
 
+      module ClassMethods
+        def after_class
+          super
+          # clean up after every class is run and force a new poltergeist instance
+          Capybara.reset_sessions!
+          Capybara.current_session.driver.restart
+        end
+      end
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
       def before_setup
         super
         r = Micron.runner.reporters.find{ |r| r.kind_of? Micron::Reporter::Poltergeist }
         page.driver.client.phantomjs_logger = r.logger
-      end
-
-      def self.after_class
-        Capybara.reset_sessions!
       end
 
       def teardown
