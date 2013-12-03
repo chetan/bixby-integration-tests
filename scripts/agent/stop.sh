6e95b23701dfe6f9a0cab996ba1b35ad2deb66c7
Chetan Sarva <chetan@pixelcop.net>
2013-12-03 17:34:53 -0500

    recreate etc on each run

--- scripts/agent/stop.sh
+++ scripts/agent/stop.sh
@@ -7,9 +7,9 @@ sudo pkill -9 -u 0 -U 0 -f bixby-agent
 sudo pkill -9 -f mon_daemon
 
 if [[ -d /opt/bixby/etc ]]; then
-  cd /opt/bixby/etc
-  sudo rm -f bixby.yml id_rsa server.pub god.d/bixby-monitoring.god
-  sudo rm -rf monitoring
+  sudo rm -rf /opt/bixby/etc
+  sudo mkdir -p /opt/bixby/etc
+  sudo chown bixby:bixby /opt/bixby/etc
 fi
 
 if [[ -d /opt/bixby/var ]]; then
--- scripts/agent/upgrade.sh
+++ scripts/agent/upgrade.sh
@@ -38,3 +38,15 @@ git pull -q
 rm -rf pkg *.gem
 gem build *.gemspec >/dev/null
 sudo $shim gem install *.gem --no-ri --no-rdoc --local >/dev/null
+
+# other agent stuff
+# init
+cd /opt/bixby-integration/src/agent/etc
+sudo cp -a bixby-god.initd /etc/init.d/bixby
+sudo chmod 755 /etc/init.d/bixby
+# god config
+sudo mkdir -p /opt/bixby/
+sudo cp -a bixby.god god.d /opt/bixby/etc/
+
+# cleanup
+sudo chown -R bixby:bixby /opt/bixby
