101b840551266be29f6114ceb76ce4356797b8ec
Chetan Sarva <chetan@pixelcop.net>
2014-02-05 14:00:37 -0500

    reset mongodb data dir

--- scripts/manager/stop.sh
+++ scripts/manager/stop.sh
@@ -9,4 +9,12 @@ cd /var/www/bixby/current
 bundle install --local > /dev/null
 rake db:drop
 redis-cli flushall > /dev/null
+
+# reset mongodb data dir
+sudo service mongodb stop
+sudo rm -rf /var/lib/mongodb
+sudo mkdir /var/lib/mongodb
+sudo chown mongodb:mongodb /var/lib/mongodb
+sudo service mongodb start
+
 rm -rf /var/www/bixby/shared/bixby/*
